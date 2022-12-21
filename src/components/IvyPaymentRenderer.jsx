638fa83400810d935ffc62942cd73179f2009f7d
Kostiantyn Sprohe <kostya.sproge@gmail.com>
2022-12-21 20:33:15 +0200

    Component improvements

--- src/components/IvyPaymentRenderer.jsx
+++ src/components/IvyPaymentRenderer.jsx
@@ -3,17 +3,16 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { PAYMENT_METHOD_FORM } from '@hyva/react-checkout/config';
 import { paymentMethodShape } from '@hyva/react-checkout/utils/payment';
 import RadioInput from '@hyva/react-checkout/components/common/Form/RadioInput';
-import RootElement from '@hyva/react-checkout/utils/rootElement';
 import { __ } from '@hyva/react-checkout/i18n';
 import useCheckoutFormContext from '@hyva/react-checkout/hook/useCheckoutFormContext';
+import { ivyPaymentLogoUrl } from '../utils';
 import useIvyPayment from '../hooks/useIvyPayment';
 import useIvyPaymentAppContext from '../hooks/useIvyPaymentAppContext';
 import useIvyPaymentCartContext from '../hooks/useIvyPaymentCartContext';
 import useIvyPaymentFormikContext from '../hooks/useIvyPaymentFormikContext';
 
 function IvyPaymentRenderer({ method, selected }) {
-  const ivyPaymentLogoUrl = `${RootElement.getFilePath()}/Esparksinc_IvyPayment/images/ivylogo.svg`;
-  const { registerPaymentAction } = useCheckoutFormContext();
+  const { registerPaymentAction, setErrorMessage } = useCheckoutFormContext();
   const methodCode = method.code;
   const { paymentSubmitHandler } = useIvyPayment();
   const { setPageLoader } = useIvyPaymentAppContext();
@@ -28,14 +27,18 @@ function IvyPaymentRenderer({ method, selected }) {
 
   const paymentSelectionHandler = useCallback(async () => {
     setPageLoader(true);
-    setFieldValue(`${PAYMENT_METHOD_FORM}.code`, methodCode);
-    await setPaymentMethod(methodCode);
+    try {
+      setFieldValue(`${PAYMENT_METHOD_FORM}.code`, methodCode);
+      await setPaymentMethod(methodCode);
+    } catch (error) {
+      setErrorMessage(__(error));
+    }
     setPageLoader(false);
   }, [methodCode, setPageLoader, setPaymentMethod, setFieldValue]);
 
   useEffect(() => {
-    registerPaymentAction('ivy', paymentSubmitHandler);
-  }, [registerPaymentAction, paymentSubmitHandler]);
+    registerPaymentAction(methodCode, paymentSubmitHandler);
+  }, [registerPaymentAction, paymentSubmitHandler, methodCode]);
 
   const radioLabel = (
     // eslint-disable-next-line jsx-a11y/label-has-associated-control
